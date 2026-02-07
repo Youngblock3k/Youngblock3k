57a3b711fe79c2b818a05d3f9625e4790719de30
Youngblock3k <Youngblock2kus@gmail.com>
2026-02-08 01:08:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const DATA = {
         name: "Justanother_game",
         age: 15,
         role: "Game / Backend Developer",
-        location: "India",
+        location: "Oslo, Norway",
         email: "just_anothergame04@gmail.com",
         bio: "Developer focused on Minecraft servers, scripting, automation, and game systems.",
         pfp: "pfp.png"
@@ -14,7 +14,7 @@ const DATA = {
         "Skript",
         "JavaScript",
         "HTML",
-        "Lua",
+        "CSS",
         "Minecraft Server Development",
         "Discord Bots",
         "Automation"
@@ -25,19 +25,25 @@ const DATA = {
             title: "Culling",
             description: "Jujutsu Kaisen-based Minecraft server with intense challenges and progression systems.",
             tags: ["Minecraft", "PvP", "Custom Systems"],
-            theme: "dark"
+            theme: "dark",
+            image: "https://cdn.discordapp.com/icons/1291508643329740941/382d5fdd1d7549d1fa73337833a9a667.png?size=4096",
+            discord: "https://discord.gg/Tnbhdjs23q"
         },
         {
-            title: "VoiidNetwork",
-            description: "Multi-mode Minecraft network including Survival, Lifesteal, and BoxMine.",
-            tags: ["Network", "Survival", "Lifesteal"],
-            theme: "purple"
+            title: "Dirtbox",
+            description: "Multi-mode Minecraft network with advanced server infrastructure and gameplay systems.",
+            tags: ["Network", "Survival", "Custom Systems"],
+            theme: "purple",
+            image: "https://cdn.discordapp.com/icons/1464667156955402499/315a3a4b3d018fa5457def07cfdbff48.png?size=4096",
+            discord: "https://discord.gg/MtCfgK9mHB"
         },
         {
-            title: "Custom Discord AI Bot",
-            description: "Discord bot with automation, moderation, and smart replies.",
-            tags: ["Discord", "Automation", "AI"],
-            theme: "blue"
+            title: "Gamestop",
+            description: "Discord Bot Store with automation, moderation, and smart features for community management.",
+            tags: ["Discord", "Automation", "Store"],
+            theme: "blue",
+            image: "https://cdn.discordapp.com/icons/1469641813425131674/880cab82b517d5676dfbd84c2131eb52.png?size=4096",
+            discord: "https://discord.gg/EPTu4uW3z4"
         }
     ]
 };
